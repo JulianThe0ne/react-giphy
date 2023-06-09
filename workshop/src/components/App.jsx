52a429c1e90347a27a887ea253f1d392b169f5f4
Julien Afonso <ju.afonso61@gmail.com>
2023-06-09 12:03:00 +0900

    react intro

---
+++ workshop/src/components/App.jsx
@@ -0,0 +1,20 @@
+// import React from 'react';
+
+// const App = (props) => {
+//   return <div>App</div>;
+// }
+// export default App;
+
+// rsc + tab => react functional Component
+// rcc + tab => react class Component
+import React, { Component } from 'react';
+
+const App = () => {
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+export default App;
--- workshop/src/index.jsx
+++ workshop/src/index.jsx
@@ -1,1 +1,24 @@
 import '../assets/stylesheets/application.scss';
+
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+// Functional Component
+const Hello = (props) => {
+  console.log(props);
+
+  const { name, age } = props;
+  return <h1>Hello I'm {name}, {age} years old 🐆!!</h1>;
+};
+
+// Class Component
+// class Hello extends React.Component {
+//   render() {
+//     const { name, age } = this.props;
+//   return <h1>Hello I'm {name}, {age} years old 🐆!!</h1>;
+//   }
+// }
+
+const rootElement = document.querySelector("#root");
+const root = createRoot(rootElement);
+root.render(<Hello name="Chieri" age="21"/>);
